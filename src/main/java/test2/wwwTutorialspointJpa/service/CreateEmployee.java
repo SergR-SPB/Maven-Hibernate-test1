2c27ae64ed8237f0213688f44a10c2963a61813b
Сергей <spbkarta@yandex.ru>
2019-05-31 02:22:02 +0400

    Обновленмия от 31.05 2.21 не рабочиее

--- src/main/java/test2/wwwTutorialspointJpa/service/CreateEmployee.java
+++ src/main/java/test2/wwwTutorialspointJpa/service/CreateEmployee.java
@@ -7,6 +7,8 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 public class CreateEmployee {
+
+
     public static void main(String[] args) {
         EntityManagerFactory emfactory =  Persistence .createEntityManagerFactory ( "Clients" );
 
